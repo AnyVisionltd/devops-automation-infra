69d450657ab11f2ec91b403750fc06ba7a17a666
itayP <itayp@anyvision.co>
2020-01-07 10:51:37 +0200

    tests/test_kafka: added basic test for consume_iter

---
+++ tests/test_kafka.py
@@ -0,0 +1,9 @@
+from devops_plugins.kafka import Kafka
+
+
+def test_kafka_consume_topic(base_config):
+    messages = list()
+    queue = ['anv.tracks.pipeng.new-tracks']
+    for msg in base_config.host.Kafka.consume_iter(queue, timeout=5, commit=True):
+        messages.append(msg)
+    return messages
