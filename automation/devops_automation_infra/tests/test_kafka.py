7cc8ef2ff45d37284b61258268b6b52430257c72
oriHab <orielh@anyvision.co>
2020-02-02 16:47:48 +0200

    test_kafka: improved kafka test
    The test is now way more comprehensive.

--- automation/devops_automation_infra/tests/test_kafka.py
+++ automation/devops_automation_infra/tests/test_kafka.py
@@ -1,11 +1,17 @@
+import logging
+
 from devops_automation_infra.plugins.kafka import Kafka
 from pytest_automation_infra.helpers import hardware_config
 
 
 @hardware_config(hardware={"host": {}})
-def test_kafka_consume_topic(base_config):
-    messages = list()
-    queue = ['anv.tracks.pipeng.new-tracks']
-    for msg in base_config.host.host.Kafka.consume_iter(queue, timeout=5, commit=True):
-        messages.append(msg)
-    return messages
+def test_tunneled_kafka(base_config):
+    kafka = base_config.hosts.host.Kafka
+    topics = kafka.get_topics()
+    logging.info(f"topics: {topics}")
+    kafka.create_topic("aaaOri")
+    topics_to_empty = ['anv.tracks.collate.new-tracks', 'anv.tracks.tracks-consumer-producer.new-tracks']
+    msg = kafka.get_message(topics_to_empty)
+    logging.error(f'msg: {msg}')
+    kafka.empty(topics_to_empty, 10)
+    kafka.delete_topic("aaaOri")
