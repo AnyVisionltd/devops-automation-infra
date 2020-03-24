d7e17de401e4cc0171c9cc8018aad2956c9e6208
Chen <camiel@anyvision.co>
2020-03-24 15:22:28 +0200

    Promethues Plugin | Support k8s
    Matching for k8s:
    1) Port
    2) DNS
    3) URL
    4) Headers

--- automation/devops_automation_infra/plugins/prometheus.py
+++ automation/devops_automation_infra/plugins/prometheus.py
@@ -1,19 +1,27 @@
 import json
 
+from base64 import b64encode
 from prometheus_http_client import Prometheus
 
 from devops_automation_infra.utils.config import prometheus_connection_config
 from automation_infra.plugins.base_plugin import TunneledPlugin
+from pytest_automation_infra import helpers
 from infra.model import plugins
 
 
 class PrometheusService(TunneledPlugin):
     def __init__(self, host):
         super().__init__(host)
-        self.DNS_NAME = host.ip
-        self.PORT = prometheus_connection_config['port']
+        self.is_k8s = helpers.is_k8s(self._host.SshDirect)
+        self.DNS_NAME = host.ip if not self.is_k8s else prometheus_connection_config['host']['k8s']
+        self.PORT = prometheus_connection_config['port']['compose'] if not self.is_k8s \
+            else prometheus_connection_config['port']['k8s']
         self.start_tunnel(self.DNS_NAME, self.PORT)
-        self._prom = Prometheus(url=f'http://localhost:{self.local_bind_port}')
+        self.url = f'{prometheus_connection_config["url"]["compose"]}:{self.local_bind_port}' if not self.is_k8s \
+            else prometheus_connection_config['url']['k8s']
+        self.headers = None if not self.is_k8s \
+            else {'Authorization': f'Basic {prometheus_connection_config["auth"]}'}
+        self._prom = Prometheus(url=self.url, headers=self.headers)
 
     def query(self, query):
         return json.loads(self._prom.query(metric=query))
--- automation/devops_automation_infra/utils/config.py
+++ automation/devops_automation_infra/utils/config.py
@@ -1,3 +1,14 @@
 prometheus_connection_config = {
-    'port': 9090
+    'host': {
+        'k8s': 'prom.anv'
+    },
+    'port': {
+        'compose': 9090,
+        'k8s': 80
+    },
+    'url': {
+        'compose': 'http://localhost',
+        'k8s': 'http://prom.anv/'
+    },
+    'auth': 'YWRtaW46UGFzc3cwcmQxMjM='
 }
