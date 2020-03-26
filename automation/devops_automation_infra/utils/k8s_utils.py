e4883836c1f0d4547a0c5a657ea1842ca1f1112e
Omri Golan <omrig@anyvision.com>
2020-03-26 12:20:53 +0200

    bug fix | k8s util used the wrong waiter
    When creating the deployment i used the wrong wait since the
    waiter would only try again if the return was not none and since we
    returned false in case of failer the waiter wouldn't try again which
    would fail the test

--- automation/devops_automation_infra/utils/k8s_utils.py
+++ automation/devops_automation_infra/utils/k8s_utils.py
@@ -1,7 +1,7 @@
-from automation_infra.utils.waiter import wait_for_predicate_nothrow
+from automation_infra.utils.waiter import wait_for_predicate
 
 
 def create_deployment_with_replicas(host, name, docker_image, amount_of_replicas):
-    wait_for_predicate_nothrow(lambda: host.K8s.create_deployment(name, docker_image), timeout=120)
+    wait_for_predicate(lambda: host.K8s.create_deployment(name, docker_image), timeout=120)
     host.K8s.scale_deployment(name, int(amount_of_replicas))
     host.K8s.expose_deployment(name)
