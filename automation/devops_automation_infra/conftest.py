a9bd1705df6c57afb0a2696a2dec7920f650129a
oriHab <orielh@anyvision.co>
2020-04-12 14:02:04 +0300

    conftest: added runtest_setup
    The goal of this function is to enable cleaning between tests based on
    implementation of clean methods by each plugin, and invoking them by the
    host, when runtest_setup is invoked by pytest.

--- automation/devops_automation_infra/conftest.py
+++ automation/devops_automation_infra/conftest.py
@@ -1,5 +1,8 @@
+import logging
 import pytest
 
+from automation_infra.utils.initializer import init_plugins as init_infra_plugins
+from devops_automation_infra.utils.initializer import init_plugins as init_devops_plugins
 from pytest_automation_infra import determine_scope
 
 
@@ -11,3 +14,14 @@ def clean_up_all_deployments_and_svcs(base_config):
     yield
     k8s.delete_deployment(name="", all=True)
     k8s.delete_svc(svc_name="", all=True)
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_runtest_setup(item):
+    logging.info("running dev-ops pre-test cleaner.")
+    hosts = item.funcargs['base_config'].hosts
+    for name, host in hosts.items():
+        # multi-threaded
+        init_infra_plugins(host)
+        init_devops_plugins(host)
+        host.clean_between_tests()
