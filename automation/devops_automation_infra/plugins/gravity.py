dc6824cab572d6f46139c8ae87c1f25149c62cd0
Omri Golan <omrig@anyvision.com>
2020-04-16 13:36:32 +0300

    gravity plugin | add node
    gravity plugin | A node to cluster
    
    We needed the ability to take a complete clean host and add them to a
    gravity cluster in order to test a full flow of installing and setting
    up our product.

--- automation/devops_automation_infra/plugins/gravity.py
+++ automation/devops_automation_infra/plugins/gravity.py
@@ -1,7 +1,9 @@
 import json
 
 from automation_infra.plugins.ssh_direct import SshDirect
+from automation_infra.utils.waiter import wait_for_predicate, wait_for_predicate_nothrow
 from infra.model import plugins
+from pytest_automation_infra.helpers import hardware_config
 
 
 class Gravity(object):
@@ -13,17 +15,45 @@ class Gravity(object):
         return json.loads(self._host.SshDirect.execute("sudo gravity status --output json"))
 
     def is_cluster_healthy(self):
-        res = self.status()
-        all_nodes = res['cluster']['nodes']
-        health_nodes_number = [node['status'] for node in all_nodes if node['status'] == "healthy"]
-        return True if len(health_nodes_number) == len(all_nodes) else False
+        return self.number_healthy_nodes() == len(self.nodes())
 
     def number_healthy_nodes(self, number_nodes_required=None):
-        res = self.status()
-        nodes = res['cluster']['nodes']
+        nodes = self.nodes()
         health_nodes_number = [node['status'] for node in nodes if node['status'] == "healthy"]
         return len(health_nodes_number)
 
+    def nodes(self):
+        res = self.status()
+        return res['cluster']['nodes']
+
+    def join_cluster(self, master_ip, join_token):
+        download_path = "/usr/local/bin/gravity"
+        download_cmd = f"sudo curl -k -H \"Authorization: Bearer {join_token}\" https://{master_ip}:32009/portal/v1/gravity -o {download_path}"
+
+        self.download_gravity(download_cmd)
+        self.gravity_make_executable(download_path)
+
+        self.join(master_ip, join_token)
+        wait_for_predicate_nothrow(lambda: self.is_cluster_healthy(), timeout=600, interval=10)
+
+    @property
+    def token(self):
+        res = self.status()
+        return res['cluster']['token']['token']
+
+    @property
+    def master_ip(self):
+        res = self.status()
+        return res['cluster']['nodes'][0]['advertise_ip']
+
+    def download_gravity(self, download_request):
+        self._host.SshDirect.execute(download_request)
+
+    def gravity_make_executable(self, gravity_path):
+            self._host.SshDirect.execute(f"sudo chmod +x {gravity_path}")
+
+    def join(self, master_ip, join_token, role='node', cloud_provider='generic'):
+        self._host.SshDirect.execute(f'sudo gravity join {master_ip} --token={join_token} --role={role} --cloud-provider={cloud_provider}')
+
 
 plugins.register('Gravity', Gravity)
-
