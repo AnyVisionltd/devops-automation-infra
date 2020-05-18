b904b4e08a00137c969d942e0b3f0e0249c0b0cd
oriHab <orielh@anyvision.co>
2020-05-18 13:21:14 +0300

    plugins/kafka, utils/rpyc_kafka_server: redesigned
        There were a few problems with the previous implementation:
        1. everything was too slow
        2. there could be only one consumer/producer/admin
    
        Re 1: kafka-python library seems to be a lot faster.
        Re 2: refactored so you just get admin/producer/consumer instances and
        therefore can create however many you want.
    plugins/kafka, utils/rpyc_kafka_server: redesigned
    
        There were a few problems with the previous implementation:
        1. everything was too slow
        2. there could be only one consumer/producer/admin
    
        Re 1: kafka-python library seems to be a lot faster.
        Re 2: refactored so you just get admin/producer/consumer instances and
        therefore can create however many you want.

---
+++ automation/devops_automation_infra/plugins/kafka.py
@@ -0,0 +1,63 @@
+import logging
+import rpyc
+
+from automation_infra.utils import waiter
+from infra.model import plugins
+from pytest_automation_infra import helpers
+
+from devops_automation_infra.utils import rpyc_kafka_server
+from devops_automation_infra.utils import kafka_client
+from devops_automation_infra.utils import container
+
+
+class Kafka(object):
+    RPYC_PORT = 18861
+
+    def __init__(self, host):
+        self._host = host
+        self.DNS = 'kafka.tls.ai' if not helpers.is_k8s(self._host.SshDirect) \
+                else 'kafka-cluster-kafka-brokers.default.svc.cluster.local'
+        self.PORT = '9092'
+        self._rpyc = None
+
+    def _start_server(self):
+        if self._rpyc is not None:
+            return self._rpyc
+        logging.debug(f"starting kafka rpyc server on {self._host.ip}")
+        self._rpyc = self._host.SSH.run_background_snippet(rpyc_kafka_server.run_kafka_rpyc_server)
+        waiter.wait_for_predicate_nothrow(lambda: self._rpyc.running(), timeout=5)
+
+    def _create_connection(self):
+        tunnel = self._host.TunnelManager.get_or_create('kafka', self._host.ip, Kafka.RPYC_PORT)
+        return waiter.wait_for_predicate_nothrow(
+            lambda: rpyc.connect(self._host.ip, tunnel.local_port, config={'allow_all_attrs': True, 'sync_request_timeout': 120}),
+            timeout=10)
+
+    def create_client(self):
+        self._start_server()
+        return kafka_client.Kafka(self._create_connection())
+
+    def ping(self):
+        return self.create_client().ping()
+
+    def reset_state(self):
+        if not helpers.is_k8s(self._host.SshDirect):
+            self.delete_storage_compose()
+        else:
+            raise Exception("Infra doesnt know how to reset_state of kafka on K8s")
+
+    def restart(self):
+        container.restart_container_by_service(self._host, "kafka")
+
+    def stop(self):
+        container.stop_container_by_service(self._host, "kafka")
+
+    def start(self):
+        container.start_container_by_service(self._host, "kafka")
+
+    def delete_storage_compose(self):
+        self._host.SshDirect.execute('rm -rf /storage/kafka/*')
+
+
+plugins.register('Kafka', Kafka)
+
--- automation/devops_automation_infra/utils/rpyc_kafka_server.py
+++ automation/devops_automation_infra/utils/rpyc_kafka_server.py
@@ -1,42 +1,31 @@
+import kafka
 import rpyc
-from confluent_kafka.cimpl import KafkaException
 from rpyc.utils.server import ThreadedServer
-from confluent_kafka import Consumer, Producer, TopicPartition
-from confluent_kafka.admin import AdminClient, NewTopic
-
-
-TIMEOUT = 10
+import logging
 
 class KafkaServer(rpyc.Service):
-    def __init__(self):
-        self._kafka_config = None
-        self._consumer = None
-        self._producer = None
-        self._admin = None
+    @staticmethod
+    def get_admin(**kwargs):
+        logging.error("getting admin")
+        return kafka.KafkaAdminClient(**kwargs)
 
-    def set_kafka_config(self, kafka_host, kafka_port, offset):
-        self._kafka_config = {'bootstrap.servers': f'{kafka_host}:{kafka_port}',
-                                   'group.id': "automation-group",
-                                   'session.timeout.ms': 6000,
-                                   'auto.offset.reset': offset}
+    @staticmethod
+    def get_producer(**kwargs):
+        """to produce: producer.send(topic, message_bytes)"""
+        logging.error("getting admin")
+        return kafka.KafkaProducer(**kwargs)
 
-    @property
-    def admin(self):
-        if self._admin is None:
-            self._admin = AdminClient(self._kafka_config)
-        return self._admin
-
-    @property
-    def consumer(self):
-        if self._consumer is None:
-            self._consumer = Consumer(self._kafka_config)
-        return self._consumer
-
-    @property
-    def producer(self):
-        if self._producer is None:
-            self._producer = Producer(self._kafka_config)
-        return self._producer
+    @staticmethod
+    def get_consumer(*topics, **kwargs):
+        """to consume:
+        for message in consumer:
+            message value and key are raw bytes -- decode if necessary!
+            e.g., for unicode: `message.value.decode('utf-8')`
+            print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                                  message.offset, message.key,
+                                                  message.value))
+        """
+        return kafka.KafkaConsumer(*topics, **kwargs)
 
     @staticmethod
     def create_list(*args):
@@ -45,53 +34,22 @@ class KafkaServer(rpyc.Service):
 
     @staticmethod
     def create_topic_object(topic_name, num_partitions=3, replication_factor=1):
-        new_topic = NewTopic(topic_name, num_partitions=num_partitions, replication_factor=replication_factor)
+        new_topic = kafka.admin.NewTopic(topic_name, num_partitions=num_partitions, replication_factor=replication_factor)
         return new_topic
 
     @staticmethod
     def create_topic_partition_object(topic, partition, offset):
-        topic_partition = TopicPartition(topic=topic, partition=partition, offset=offset)
+        topic_partition = kafka.TopicPartition(topic=topic, partition=partition, offset=offset)
         return topic_partition
 
-    def num_topics(self):
-        return len(self.topic_names())
 
-    def topic_names(self):
-        topics = self.admin.list_topics(timeout=TIMEOUT).topics
-        return [k for k, v in topics.items()]
-
-    def delete_topics(self, topic_names):
-        fs = self.admin.delete_topics(topic_names)
-        for topic_name, f in fs.items():
-            try:
-                f.result()
-                print(f'Topic {topic_name} deleted')
-            except KafkaException as e:
-                print(f"caught exception deleting topic {topic_name}: {e}")
-                return e
-        return True
-
-    def create_topics(self, *topic_names):
-        topics_to_create = []
-        for topic_name in topic_names:
-            topic = self.create_topic_object(topic_name)
-            topics_to_create.append(topic)
-        fs = self.admin.create_topics(topics_to_create, request_timeout=TIMEOUT, operation_timeout=TIMEOUT)
-        for topic, f in fs.items():
-            try:
-                f.result()
-                print(f'Topic {topic} created')
-            except KafkaException as e:
-                if e.args[0].name() != 'TOPIC_ALREADY_EXISTS':
-                    print(e.args[0].code())
-                    print(e.args[0].name())
-                    return e
-        return True
+def run_kafka_rpyc_server(port=18861, protocol_config={'allow_public_attrs': True}):
+    ThreadedServer(
+        KafkaServer,
+        port=port,
+        protocol_config=protocol_config
+    ).start()
 
 
 if __name__ == "__main__":
-    ThreadedServer(
-        KafkaServer,
-        port=18861,
-        protocol_config={'allow_public_attrs': True}
-    ).start()
+    run_kafka_rpyc_server()
