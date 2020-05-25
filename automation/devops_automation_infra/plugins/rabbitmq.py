b429461ceffa9f7c4ef1b99732a5b93a61268152
avidani@anyvision.co <avidani@anyvision.co>
2020-05-25 18:10:57 +0300

    changed the rabbitmq to not be a tunneled plugin plus * fixed connection to use TunnelManager * added an assert to verify functionality to test recieved message from rabbitmq * changed class from TunneledPlugin to object * change connection behaviour to be rested with each new request instead of reuse old connections

--- automation/devops_automation_infra/plugins/rabbitmq.py
+++ automation/devops_automation_infra/plugins/rabbitmq.py
@@ -5,25 +5,28 @@ from automation_infra.plugins.base_plugin import TunneledPlugin
 from pytest_automation_infra.helpers import hardware_config
 
 
-class RabbitMQ(TunneledPlugin):
+class RabbitMQ(object):
     def __init__(self, host):
-        super().__init__(host)
+        self._host = host
         self.DNS_NAME = 'rabbitmq.tls.ai'
         self.PORT = 5672
-        self._connection = None
 
     @property
     def connection(self):
-        if self._connection is None:
-            self._connection = self._get_connection()
-        return self._connection
+        return self._get_connection()
+
+    @property
+    def tunnel(self):
+        return self._host.TunnelManager.get_or_create('consul', self.DNS_NAME, self.PORT)
 
     def _get_connection(self):
         try:
-            self.start_tunnel(self.DNS_NAME, self.PORT)
+            _tunnel = self.tunnel
+            _tunneled_host = _tunnel.host_port[0]
+            _tunneled_port = _tunnel.host_port[1]
             rmq_credentials = pika.PlainCredentials('user', 'password')
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
-                                                                           port=self.local_bind_port,
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=_tunneled_host,
+                                                                           port=_tunneled_port,
                                                                            credentials=rmq_credentials))
             return connection
         except Exception as e:
@@ -31,13 +34,14 @@ class RabbitMQ(TunneledPlugin):
 
     def verify_functionality(self):
         queue_name = 'aut_test_rmq'
-        rmq_channel = self.connection.channel()
+        rmq_conn = self.connection
+        rmq_channel = rmq_conn.channel()
         rmq_channel.queue_declare(queue=queue_name)
         rmq_channel.basic_publish(exchange='',
                                   routing_key=queue_name,
                                   body='Hello World')
         res = rmq_channel.basic_get(queue=queue_name, auto_ack=False)
-        print(res)
+        assert res[2] == b'Hello World'
         rmq_channel.queue_delete(queue=queue_name)
         logging.info("<<<<<<<RABBITMQ PLUGIN FUNCTIONING PROPERLY>>>>>>>>>>>>>")
 
