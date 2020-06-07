ad2bab25ed572772f434f55f191da0208547ee3a
Solganik Alexander <alexanders@anyvision.co>
2020-06-07 10:37:38 +0300

    consul: remove unneded imports

--- automation/devops_automation_infra/plugins/consul.py
+++ automation/devops_automation_infra/plugins/consul.py
@@ -1,9 +1,6 @@
-import logging
 import json
-import sshtunnel
 import base64
 import consul
-from munch import Munch
 from automation_infra.plugins import tunnel_manager
 from infra.model import plugins
 from pytest_automation_infra import helpers
