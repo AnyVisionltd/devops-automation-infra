2e79fb256bd2aaae35f1b58856affddaca669d05
Nadav Eidelstein <nadav@anyvision.co>
2020-07-12 17:50:19 +0300

    add test_get_host_ip

---
+++ automation/devops_automation_infra/tests/docker_tests/test_get_host_ip.py
@@ -0,0 +1,11 @@
+import logging
+
+from utils.host import get_host_ip
+from pytest_automation_infra.helpers import hardware_config
+
+
+@hardware_config(hardware={"host": {}})
+def test_get_host_ip(base_config):
+    ip = get_host_ip(base_config.hosts.host)
+    logging.info(f"get_host_ip got ip {ip}")
+    assert ip
