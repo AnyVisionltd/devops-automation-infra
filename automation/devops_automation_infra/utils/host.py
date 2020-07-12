5b9e48b5f63dc160389c74d3d0a9c9ad574f5e5d
Nadav Eidelstein <nadav@anyvision.co>
2020-07-12 17:36:57 +0300

    print network interface used in get_host_ip()

--- automation/devops_automation_infra/utils/host.py
+++ automation/devops_automation_infra/utils/host.py
@@ -1,8 +1,13 @@
+import logging
+
+
 def get_default_net_interface(host):
     cmd = "ip route | grep default | cut -d' ' -f 5"
     return host.SshDirect.execute(cmd).strip()
 
 
 def get_host_ip(host):
-    cmd = f"ip addr show | grep '{get_default_net_interface(host)}' | grep  -Po 'inet \\K(\\d+\\.?){{4}}'"
+    network_iface = get_default_net_interface(host)
+    logging.debug(f"using network_interface {network_iface}")
+    cmd = f"ip addr show | grep '{network_iface}' | grep  -Po 'inet \\K(\\d+\\.?){{4}}'"
     return host.SshDirect.execute(cmd).strip()
