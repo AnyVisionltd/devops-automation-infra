0710bb9b93a290fb8929006b065f8ea742c135ec
Viktor <viktorv@anyvision.co>
2020-12-13 21:14:49 +0200

    Add external host IP getter
    Added external host IP getter for camera-service direct requests. It used to take internal IP which was the reason of connection error.

--- automation/devops_automation_infra/utils/host.py
+++ automation/devops_automation_infra/utils/host.py
@@ -1,3 +1,6 @@
+from requests import get
+
+
 def get_default_net_interface(host):
     cmd = "ip route | grep default | cut -d' ' -f 5"
     return host.SshDirect.execute(cmd).strip()
@@ -6,3 +9,7 @@ def get_default_net_interface(host):
 def get_host_ip(host):
     cmd = f"ip addr show | grep '{get_default_net_interface(host)}' | grep  -Po 'inet \\K(\\d+\\.?){{4}}'"
     return host.SshDirect.execute(cmd).strip()
+
+
+def get_external_host_ip():
+    return get('https://api.ipify.org').text
