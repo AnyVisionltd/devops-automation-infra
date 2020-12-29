AnyVisionltd/devops-automation-infra

commit 1f2f1f88a893ada97757e5da04ac737e036e6320

tunnel: fix the ip of the tunnel we create.
Currently the endpoint of the tunnel is defined as the host under test,
this is incorrect as the endpoint of the tunnel is actually "local" to
that host (it is automation infra container that is running kafka proxy
server).
This does not work on AWS, where the external IP of the host cannot be
used for host to host communication. (external IP is not resolvable from
within the host itself), and therefore tunnel target cannot be the
external ip of the host

automation/devops_automation_infra/plugins/kafka.py
@@ -37,7 +37,7 @@ class Kafka(object):
             raise
 
     def _create_connection(self):
-        tunnel = self._host.TunnelManager.get_or_create('kafka', self._host.ip, Kafka.RPYC_PORT)
+        tunnel = self._host.TunnelManager.get_or_create('kafka', "localhost", Kafka.RPYC_PORT)
         return waiter.wait_for_predicate_nothrow(
             lambda: rpyc.connect(*tunnel.host_port, config={'allow_all_attrs': True, 'sync_request_timeout': 120}),
             timeout=10)

commit 333d809986114e7cca42ba00544ced68289af579

kafka: fix tunnel endpoint of kafka rpyc proxy.
Kafka plugin works in the following way:
1) We have a python rpyc server running on automation proxy container
(host under test)
2) Python client that runs on host running test connects to that python
code (rpyc kafka) via ssh runnel (which is also running on automation
proxy).
3) the target of the tunnel was set to localhost, which means that
automation_proxy was trying to open a connection to "localhost". This is
correct, but!!

We had a corrputed /etc/hosts which did not contain an entry for
localhost, which caused localhost not been resolved from
automation_proxy container.
This caused tunnel not been able to be established to the "kafka python
rpyc" server and all kafka related tests to fail on such setups.

Now to be safe we just change it to 127.0.0.1 to avoid DNS issues
enterely.

Fixes: 1f2f1f8 ("tunnel: fix the ip of the tunnel we create.")

automation/devops_automation_infra/plugins/kafka.py
@@ -37,7 +37,7 @@ class Kafka(object):
             raise
 
     def _create_connection(self):
-        tunnel = self._host.TunnelManager.get_or_create('kafka', "localhost", Kafka.RPYC_PORT)
+        tunnel = self._host.TunnelManager.get_or_create('kafka', "127.0.0.1", Kafka.RPYC_PORT)
         return waiter.wait_for_predicate_nothrow(
             lambda: rpyc.connect(*tunnel.host_port, config={'allow_all_attrs': True, 'sync_request_timeout': 120}),
             timeout=10)