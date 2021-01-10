d04515abf3170c4339635eb45ce2c72ace7bea06
oriHab <orielh@anyvision.co>
2021-01-10 11:48:27 +0200

    devops_proxy_container pytest plugin added
    Previously the automation_proxy was rolled up inside the
    pytest_autoamtion_infra plugin (in autaomtaion-infra) and would run no
    matter what HUT we were using.
    This is a bad idea because there could be tests in which we dont want
    the proxy container to run, or for example on an edge device where the
    is no docker.
    
    So we decoupled the proxy container from automation-infra, and put the
    logic in the devops infra.
    
    This is the pytest_plugin which needs to be invoked to get the
    automation_proxy container to run on the HUT.

---
+++ automation/devops_automation_infra/plugins/proxy_container.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import subprocess
+
+from automation_infra.utils import waiter
+from infra.model import plugins
+
+from automation_infra.plugins.ssh_direct import SshDirect, SSHCalledProcessError
+from devops_automation_infra.plugins.docker import Docker
+
+
+def _memoize(function):
+    from functools import wraps
+    memo = {}
+
+    @wraps(function)
+    def wrapper(*args):
+        if args in memo:
+            return memo[args]
+        else:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+
+    return wrapper
+
+class ProxyContainer(object):
+
+    def __init__(self, host):
+        self._host = host
+        self._ssh_direct = self._host.SshDirect
+        self.docker = self._host.Docker
+        self._docker_bin_path = self.docker._docker_bin_path()
+
+    @_memoize
+    def _automation_proxy_version(self):
+        version_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../proxy_container/version.sh")
+        return subprocess.check_output([version_file]).split()[0].decode()
+
+    @property
+    def running(self):
+        return bool(self._ssh_direct.execute(f"{self._docker_bin_path} ps -aq --filter 'name=automation_proxy'"))
+
+    def check_for_legacy_containers(self):
+        ssh = self._ssh_direct
+        container_names = ssh.execute("%s ps | awk '{print $NF}' | grep compose || true " % self._docker_bin_path).split()
+        if not container_names:
+            return
+        common_prefix = os.path.commonprefix(container_names)
+        if not common_prefix or common_prefix[-1] not in '_-':
+            raise Exception(f"Found containers with different prefixes: {container_names}. "
+                            f"Please prune accordingly and rerun test.")
+
+    def run(self):
+        self.kill()
+        self.check_for_legacy_containers()
+        ssh_direct = self._ssh_direct
+
+        self.docker.login()
+
+        logging.debug("running docker")
+        run_cmd = f'{self._docker_bin_path} run -d --rm ' \
+                  f'--volume=/tmp/automation_infra/:/tmp/automation_infra ' \
+                  f'--volume=/etc/hosts:/etc/hosts ' \
+                  f'--volume=/var/log/journal:/var/log/journal ' \
+                  f'--volume=/storage/logs:/storage/logs ' \
+                  f'--privileged ' \
+                  f'--network=host ' \
+                  f'--name=automation_proxy gcr.io/anyvision-training/automation-proxy:{self._automation_proxy_version()}'
+        try:
+            ssh_direct.execute(run_cmd)
+            waiter.wait_for_predicate(lambda: self.running)
+        except SSHCalledProcessError as e:
+            if "endpoint with name automation_proxy already exists in network host" in e.stderr:
+                ssh_direct.execute(f"{self._docker_bin_path} network disconnect --force host automation_proxy")
+                ssh_direct.execute(run_cmd)
+            else:
+                raise e
+        logging.debug("docker is running")
+
+    def kill(self):
+        if not self.running:
+            logging.debug("nothing to remove")
+            return
+        logging.debug("trying to remove docker container")
+        self._ssh_direct.execute(f"{self._docker_bin_path} kill automation_proxy")
+        waiter.wait_for_predicate(lambda: not self.running)
+        logging.debug("removed successfully!")
+
+    def restart(self):
+        self.run()
+
+
+plugins.register("ProxyContainer", ProxyContainer)
---
+++ automation/devops_proxy_container/__init__.py
@@ -0,0 +1,47 @@
+import logging
+import re
+
+import pytest
+
+from automation_infra.utils import waiter
+from devops_automation_infra.plugins.proxy_container import ProxyContainer
+from devops_automation_infra.plugins.ssh import SSH
+from devops_automation_infra.plugins.tunnel_manager import TunnelManager
+
+
+def pytest_addhooks(pluginmanager):
+    from devops_proxy_container import hooks
+    pluginmanager.add_hookspecs(hooks)
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_after_base_config(base_config, request):
+    for host in base_config.hosts.values():
+        logging.info("deploying proxy_container on remote")
+        host.ProxyContainer.run()
+        logging.info("waiting for SSH connection to proxy_container")
+        waiter.wait_nothrow(lambda: host.SSH.connect(port=host.tunnelport), timeout=60)
+    # import pdb; pdb.set_trace()
+    # Install product devops_docker_installer is invoked
+    request.config.hook.pytest_after_proxy_container(base_config=base_config, request=request)
+
+
+def pytest_clean_between_tests(host, item):
+    host.TunnelManager.clear()
+    host.ProxyContainer.restart()
+    waiter.wait_nothrow(host.SSH.connect, timeout=30)
+
+
+def pytest_download_logs(host, dest_dir):
+    host.SshDirect.execute('sudo chmod ugo+rw /tmp/automation_infra && '
+                           'docker logs automation_proxy &> /tmp/automation_proxy.log && '
+                           'sudo mv /tmp/automation_proxy.log /storage/logs/automation_proxy.log')
+    dest_gz = '/tmp/automation_infra/logs.tar.gz'
+    host.SSH.compress("/storage/logs/", dest_gz)
+    host.SSH.download(re.escape(dest_dir), dest_gz)
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_after_test(item, base_config):
+    for host in base_config.hosts.values():
+        host.ProxyContainer.kill()
