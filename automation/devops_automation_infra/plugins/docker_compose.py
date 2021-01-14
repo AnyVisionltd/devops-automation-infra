2f84e9ad80b376540fadc856b61cec8b06f3c1bd
Solganik Alexander <alexanders@anyvision.co>
2021-01-14 20:34:20 +0200

    compose: add functionality to replace service environment variabes on the fly
    Sometimes it is needed to re-run the same service of the compose but
    with slightly different environment variables. So the process of doing
    it is the following:
    
    1) get compose path of the compose service
    2) kill the old one
    3) run new one with adjusted environment variables

--- automation/devops_automation_infra/plugins/docker_compose.py
+++ automation/devops_automation_infra/plugins/docker_compose.py
@@ -5,7 +5,7 @@ from automation_infra.utils.waiter import wait_for_predicate
 from infra.model import plugins
 from automation_infra.plugins.ssh_direct import SshDirect, SSHCalledProcessError
 from pytest_automation_infra.helpers import hardware_config
-
+import packaging.version
 
 class DockerCompose(object):
 
@@ -43,6 +43,47 @@ class DockerCompose(object):
                 raise Exception("unable to find the service " + container +"\nmsg: " + e.stderr)
             else:
                 raise Exception(e.stderr)
+
+    @property
+    def version(self):
+        cmd = f"{self.compose_bin_path} version --short"
+        version = self._ssh_direct.execute(cmd).strip()
+        return packaging.version.parse(version)
+
+
+    def path_from_container_id(self, container_id):
+        labels = self._host.Docker.labels(container_id)
+        compose_workdir = labels.get("com.docker.compose.project.working_dir", None)
+        compose_config = labels.get("com.docker.compose.project.config_files") 
+        if compose_workdir is None:
+            # This feature is only enabled since 1.25.2 (Jan 2020)
+            if self.version < packaging.version.Version("1.25.2"):
+                logging.error("Compose version is outdated!!!! update docker-compose")
+            raise Exception(f"service {container_id} not created by compose")
+
+        # Now depends where compose was started the "compose filename" can be not relative to workdir
+        # So now lets start looking and their shared ancestor
+        compose_file = compose_config.split("/")[-1]
+        return f"{compose_workdir}/{compose_file}"
+
+    def purge_service(self, compose_file_path, service_name):
+        cmd = f"{self.compose_bin_path} -f {compose_file_path} rm -s -f {service_name}"
+        self._ssh_direct.execute(cmd)
+
+    def run_sevice_with_environment(self, compose_file_path, service_name, environment_variables, doker_name=None):
+        env_pairs = " ".join(f"-e {k}={v}" for k, v in environment_variables.items())
+        name_cmd = f"--name {doker_name}" if doker_name is not None else ""
+        cmd = f"{self.compose_bin_path} -f {compose_file_path} run --no-deps --use-aliases --service-ports -d {name_cmd} {env_pairs} {service_name}"
+        self._ssh_direct.execute(cmd)
+
+    def service_image_fqdn(self, compose_file_path, service_name):
+        cmd = f"{self.compose_bin_path} -f {compose_file_path} images -- {service_name} | tail -n -1"
+        image_descriptor = self._ssh_direct.execute(cmd).strip().split()
+        return f"{image_descriptor[1]}:{image_descriptor[2]}"
+
+    def adjust_service_environment(self, compose_file_path, service_name, environment_variables, doker_name=None):
+        self.purge_service(compose_file_path, service_name)
+        self.run_sevice_with_environment(compose_file_path, service_name, environment_variables, doker_name=doker_name)
 
 
 plugins.register("DockerCompose", DockerCompose) 
