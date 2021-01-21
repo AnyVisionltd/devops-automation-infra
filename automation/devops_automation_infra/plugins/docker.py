AnyVisionltd/devops-automation-infra

commit 1fdc53fdcbeb8399aafe8af2d51e429e42e4a15c

docker: add several usefull methods to docker plugin

automation/devops_automation_infra/plugins/docker.py
@@ -294,4 +294,25 @@ class Docker(object):
         logpath = self.inspect(container_name)['LogPath']
         return self._ssh_direct.execute(f'truncate -s 0 {logpath}')
 
+    def pull(self, image_fqdn):
+        cmd = f"{self._docker_bin} pull {image_fqdn}"
+        return self._ssh_direct.execute(cmd)
+
+    def tag(self, image_name, new_image_name):
+        cmd = f"{self._docker_bin} tag {image_name} {new_image_name}"
+        return self._ssh_direct.execute(cmd)
+
+    def rmi(self, image_name):
+        cmd = f"{self._docker_bin} rmi {image_name}"
+        return self._ssh_direct.execute(cmd)
+
+    def image_ids(self, image_regexp):
+        cmd = f"{self._docker_bin} images -q  --filter=reference='*{image_regexp}*'"
+        return self._ssh_direct.execute(cmd).strip().split('\n')
+
+    def image_inspect(self, image_id):
+        cmd = f"{self._docker_bin} insect {image_id}"
+        return json.loads(self._ssh_direct.execute(cmd).strip())[0]
+
+
 plugins.register("Docker", Docker)

commit 1b8805ebc5a57703273b054aaa8b213124a8880f

infra: remove duplicate function.
This commit fixes: 1fdc53fd

automation/devops_automation_infra/plugins/docker.py
@@ -314,10 +314,6 @@ class Docker(object):
         cmd = f"{self._docker_bin} images -q  --filter=reference='*{image_regexp}*'"
         return self._ssh_direct.execute(cmd).strip().split('\n')
 
-    def image_inspect(self, image_id):
-        cmd = f"{self._docker_bin} insect {image_id}"
-        return json.loads(self._ssh_direct.execute(cmd).strip())[0]
-
     def labels(self, container_id):
         return self.inspect(container_id)['Config']['Labels']
 

automation/devops_automation_infra/plugins/docker_compose.py
@@ -89,7 +89,7 @@ class DockerCompose(object):
         # If restart policy is not defined lets try to figure it out
         if restart_policy is None:
             docker_id = self.service_docker_id(compose_file_path, service_name)
-            restart_policy = self._host.Docker.image_inspect(docker_id)['HostConfig']['RestartPolicy']['Name']
+            restart_policy = self._host.Docker.inspect(docker_id)['HostConfig']['RestartPolicy']['Name']
         self.purge_service(compose_file_path, service_name)
         self.run_sevice_with_environment(compose_file_path, service_name, environment_variables, doker_name=doker_name, restart_policy=restart_policy)
 