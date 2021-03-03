55fc1a242f28d9d8121db8874a3563903b6e7c89
oriHab <orielh@anyvision.co>
2021-03-03 15:37:24 +0200

    add sudo to all kubectl commands

--- automation/devops_automation_infra/k8s_plugins/kubectl.py
+++ automation/devops_automation_infra/k8s_plugins/kubectl.py
@@ -13,15 +13,15 @@ class Kubectl:
     @property
     def _tunnel(self):
         ssh = self._master.SshDirect
-        svc_ip = ssh.execute('kubectl get svc kubernetes -o jsonpath={.spec.clusterIP}')
-        svc_port = ssh.execute('kubectl get svc kubernetes -o jsonpath={.spec.ports[0].port}')
+        svc_ip = ssh.execute('sudo kubectl get svc kubernetes -o jsonpath={.spec.clusterIP}')
+        svc_port = ssh.execute('sudo kubectl get svc kubernetes -o jsonpath={.spec.ports[0].port}')
         tunnel = self._master.TunnelManager.get_or_create('kubectl', svc_ip, svc_port, ssh.get_transport())
         return tunnel
 
     def _create_config(self, **kwargs):
         ssh = self._master.SshDirect
         api_token = kwargs.pop("api_token",
-                               ssh.execute('''kubectl get secrets -n kube-system -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 --decode'''))
+                               ssh.execute('''sudo kubectl get secrets -n kube-system -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 --decode'''))
         tunnel = self._tunnel
         config = kubernetes.client.Configuration()
         config.host = f"https://{tunnel.local_endpoint}"
