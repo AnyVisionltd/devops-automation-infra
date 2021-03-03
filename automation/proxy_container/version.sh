fff3a21ae5dfbdc25ae447f7cd747d5f2299437d
Aharon <aharonr@anyvision.co>
2021-03-03 16:45:14 +0200

    proxy-daemonset: adjust version script to MAC OSX
      md5sum output on MAC it's a bit different
      so adjust daemonset proxy image to MAC OSX
    
    rancher-plugin: added kubectl pod nlabel selector
    
      added pod label selector filter

--- automation/proxy_container/version.sh
+++ automation/proxy_container/version.sh
@@ -7,7 +7,12 @@ function docker_tag () {
 
     HASH_FILES=(${BASH_SOURCE[0]} ${dockerfile} $current_dir/requirements.txt $current_dir/docker_user.pem $current_dir/docker_user.pem.pub)
     HASH_FILES+=($(find $current_dir/etc/ -type f))
-    local files_sum="$(md5sum -- ${HASH_FILES[@]} | awk {'print $1'})"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        #if Running from Mac OSX the md5sum command output is a bit different
+      local files_sum="$(md5sum -- ${HASH_FILES[@]} | awk {'print $4'})"
+    else
+      local files_sum="$(md5sum -- ${HASH_FILES[@]} | awk {'print $1'})"
+    fi
     local total_sum="$(echo -n $files_sum | md5sum | awk '{print substr($1,1,12)}')"
     echo $total_sum
 }
