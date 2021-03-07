0f34bd29a1e87888bc358fa69537250ea16ec49c
Alexander Solganik <49605589+sashas-anyvision@users.noreply.github.com>
2021-03-07 16:18:14 +0200

    Revert "Revert "Hab/v3 infra""

---
+++ run/v3/local.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "-h" ]; then
+  printf "\nWill run supplied test using locally configured hardware on hardware.yaml\n"
+  printf "\nExample usage:\n./run/aws.sh /path/to/tests --any --other --pytest --flags --you --like\n\n"
+  exit 0
+fi
+
+script_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+prefix="$script_dir/../../../automation-infra/containerize.sh python -m pytest -p pytest_subprocessor --sf=\"-p pytest_automation_infra \" -s  --noconftest"
+echo "running command: $prefix $@"
+sleep 3
+$prefix $@
