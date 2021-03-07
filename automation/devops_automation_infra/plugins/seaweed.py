450603670977300bf48fcc002db9280a9ee8d640
Yoni Nakache <45972051+LazyDBA247-Anyvision@users.noreply.github.com>
2021-03-07 19:17:10 +0200

    now with 50% chance of working :)

--- automation/devops_automation_infra/plugins/seaweed.py
+++ automation/devops_automation_infra/plugins/seaweed.py
@@ -76,7 +76,7 @@ class Seaweed(ResourceManager):
             "tr -d '>'",
             "sed '/^[[:space:]]*$/d'",
             "sed 's/^ *//'",
-            "xargs -I{} echo -e 'lock\\ns3.bucket.delete -name={}\\nunlock\\n'",
+            "xargs -I{} echo 'lock; s3.bucket.delete -name={}; unlock'",
             weed_shell
         ])
        
