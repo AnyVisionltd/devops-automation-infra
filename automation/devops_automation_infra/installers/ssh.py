aa2181ca16ed5e010b68cc6a69792e46523708cb
oriHab <orielh@anyvision.co>
2021-03-16 16:40:07 +0200

    installers/ssh.py: register to setup stage
    Because clean_between_tests disconnects SshDirect plugin, it is
    necessary to reconnect in the setup stage.
    
    Previously this worked because the docker.py installer did it, and it
    still will need to, but tests which dont invoke the "docker" installer
    (we dont have many right now) would have failed. Now they will pass.

--- automation/devops_automation_infra/installers/ssh.py
+++ automation/devops_automation_infra/installers/ssh.py
@@ -3,14 +3,18 @@ import gossip
 
 
 @gossip.register('session', tags=['ssh'], provides=['ssh'])
-def ssh_direct_connect(host, request):
+def ssh_direct_connect_session(host, request):
+    add_ssh_agent(host)
     init_host_ssh_direct(host)
     mkdir_infra(host)
 
 
-def init_host_ssh_direct(host):
+def add_ssh_agent(host):
     if host.pkey:
         host.add_to_ssh_agent()
+
+
+def init_host_ssh_direct(host):
     logging.info(f"[{host}] waiting for ssh connection...")
     host.SshDirect.connect(timeout=60)
     logging.info(f"[{host}] success!")
@@ -18,3 +22,8 @@ def init_host_ssh_direct(host):
 
 def mkdir_infra(host):
     host.SshDirect.execute("mkdir -p -m 777 /tmp/automation_infra")
+
+
+@gossip.register('setup', tags=['ssh'], provides=['ssh'])
+def ssh_direct_connect_setup(host, request):
+    init_host_ssh_direct(host)
