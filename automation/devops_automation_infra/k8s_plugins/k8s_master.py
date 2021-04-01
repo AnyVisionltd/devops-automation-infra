81465a8118be15b7f88fb1a1436b353614f2ee87
Guy Klein <guyk@anyvision.co>
2021-04-01 16:05:57 +0300

    K8s Master: raise and error when masters aren't available
    Raise an indicative message when masters aren't available
    instead of returning an empty list

--- automation/devops_automation_infra/k8s_plugins/k8s_master.py
+++ automation/devops_automation_infra/k8s_plugins/k8s_master.py
@@ -1,3 +1,5 @@
+import logging
+
 from automation_infra.utils import waiter
 from infra.model import cluster_plugins
 from automation_infra.plugins.ssh_direct import SshDirect
@@ -8,13 +10,17 @@ class K8SMaster:
         self._cluster = cluster
 
     def __call__(self):
-        return next(iter(self.list_masters()))
+        masters = self.list_masters()
+        if len(masters) > 0:
+            return next(iter(masters))
+        else:
+            raise Exception("Couldn't find running masters nodes")
 
     def list_masters(self):
         masters = []
         for host in self._cluster.hosts.values():
             try:
-                waiter.wait_nothrow(lambda: host.SshDirect.execute("sudo kubectl get po"), timeout=100)
+                waiter.wait_nothrow(lambda: host.SshDirect.execute("sudo kubectl get po"), timeout=150)
                 masters.append(host)
             except:
                 continue
