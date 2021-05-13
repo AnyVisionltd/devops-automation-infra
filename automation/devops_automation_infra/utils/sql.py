ed835a639a766c0230acf309f5f90152fcc4912d
Guy Klein <guyk@anyvision.co>
2021-05-13 12:31:51 +0300

    Automation: Added sql utils
    Added execute and fetch sql util functions with gets a db connection
    without considering if the connection is from postgresql or memsql
    also added postgres and memsql wrapper utils

---
+++ automation/devops_automation_infra/utils/sql.py
@@ -0,0 +1,14 @@
+
+
+
+def execute(connection, query):
+    with connection.cursor() as c:
+        res = c.execute(query)
+    connection.commit()
+    return res
+
+
+def fetchall(connection, query):
+    with connection.cursor() as c:
+        c.execute(query)
+        return c.fetchall()
