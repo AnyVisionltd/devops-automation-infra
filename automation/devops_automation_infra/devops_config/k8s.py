e8bb4c39e29f9a1a027290e98747a5b9f5a28e0b
Guy Klein <guyk@anyvision.co>
2021-06-21 11:34:25 +0300

    Config: Added k8s master and node configs
    The config includes roles and labels of k8s master and node
    This centralize the config in one file, so if we want to add/change
    labels we can do it from one place.
    
    The config can be imported from higher deployments whic include
    devops services.

---
+++ automation/devops_automation_infra/devops_config/k8s.py
@@ -0,0 +1,10 @@
+MASTER = {
+    "role": "master",
+    "labels": {
+        "monitor": "true", "memsql": "true", "postgres": "true", "sw-volume": 'true', "seaweedfs-postgres": "true"
+    }
+}
+
+NODE = {
+    "role": "node"
+}
